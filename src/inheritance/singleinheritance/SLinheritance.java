51bb5edda1e9260d9b920279893280f19337c526
Yogeshwari255 <yogeshwaria05@gmail.com>
2025-08-10 10:27:07 +0530

    java

--- src/inheritance/singleinheritance/SLinheritance.java
+++ src/inheritance/singleinheritance/SLinheritance.java
@@ -3,10 +3,10 @@ package inheritance.singleinheritance;
 public class SLinheritance {
 
 	public static void main(String[] args) {
-		Student student = new Student("yogi", "6060606060", "chennai", 9888878787l, 9, "xxx College");
+		// TODO Auto-generated method stub
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
 		System.out.println(student);
 
 	}
-
 
 }
